a09235ed439b9d6f4a283fcf0f0208561ce4f3db
den_po <jdp@bk.ru>
2015-12-05 00:38:20 +0300

    sourceforge

--- sarsetup.bat
+++ sarsetup.bat
@@ -82,6 +82,7 @@ function DoRules {
   addrules "Somoto"  "O=SOMOTO LIMITED, L=TEL AVIV, S=TEL AVIV, C=IL"
   addrules "SourceForge"  "O=DOVE MAX (NEW MEDIA HOLDINGS LTD.), L=TEL AVIV, C=IL"
   addrules "SourceForge"  "O=IC FORGE, L=TEL AVIV, S=ISRAEL, C=IL"
+  addrules "SourceForge"  "O=SPEEDYMAX (NEW MEDIA HOLDINGS LTD), L=TEL AVIV, C=IL"
   addrules "SourceForge"  "O=THEORY MEDIA (FRIED COOKIE LTD), L=TEL AVIV, C=IL"
   addrules "Подписано O=SKYMONK SOLUTIONS LIMITED, L=TORTOLA, S=TORTOLA, C=VG"  "O=SKYMONK SOLUTIONS LIMITED, L=TORTOLA, S=TORTOLA, C=VG"
   addrules "Softdeluxe"  "O=Softdeluxe Ltd., L=Dubna, S=Moscow region', C=RU"
